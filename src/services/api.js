e6bf52524d9eb28ea611e29d052dac61eabc0687
Theusfilipe <theusfilipe@gmail.com>
2020-03-25 16:16:05 -0300

    updated the baseURL to heruko

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'https://mercado-mais-backend.now.sh/3000',
+    baseURL: 'https://mercado-mais-backend.herokuapp.com/',
 });
 
 
